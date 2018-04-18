339002ea5b0610a91002660640621e94cd6288cd
OscarJunior <oscar.jr.gonzales.flores@gmail.com>
2018-04-18 09:28:03 +0200

    draw first graph with leaflet

---
+++ src/index.js
@@ -0,0 +1,22 @@
+var QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/' +
+    'summary/all_day.geojsonp';
+
+var quakes = Rx.Observable.interval(5000).flatMap(function () {
+        return Rx.DOM.jsonpRequest({
+            url: QUAKE_URL,
+            jsonpCallback: 'eqfeed_callback'
+        }).retry(3);
+    })
+    .flatMap(function (result) {
+        return Rx.Observable.from(result.response.features);
+    })
+    .distinct(function (quake) {
+        return quake.properties.code;
+    });
+
+quakes.subscribe(function (quake) {
+    var coords = quake.geometry.coordinates;
+    var size = quake.properties.mag * 10000;
+
+    L.circle([coords[1], coords[0]], size).addTo(map);
+});
